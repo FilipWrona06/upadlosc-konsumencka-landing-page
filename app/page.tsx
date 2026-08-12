859b198a068af34da45ce6c3201aa5b94ef20871
FilipWrona06 <f.w9@interia.pl>
2026-08-12 15:03:35 +0200

    krótsza wersja

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 import { SiteHeader } from "@/components/landing/site-header"
 import { Hero } from "@/components/landing/hero"
-import { HowIHelp } from "@/components/landing/how-i-help"
-import { WhatIs } from "@/components/landing/what-is"
-import { Services } from "@/components/landing/services"
 import { Process } from "@/components/landing/process"
 import { Pricing } from "@/components/landing/pricing"
 import { ForWhom } from "@/components/landing/for-whom"
@@ -21,9 +18,6 @@ export default function Page() {
       <Pricing />
       <WhyUs />
       <Faq />
-      <HowIHelp />
-      <WhatIs />
-      <Services />
       <Contact />
       <SiteFooter />
     </main>
